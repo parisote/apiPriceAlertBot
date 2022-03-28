194031096c332efaf66ccb9f751650828a692c46
parisote <detomas@gmail.com>
2022-03-28 17:49:02 -0300

    feat: eslint emitter

--- src/controllers/price.controller.js
+++ src/controllers/price.controller.js
@@ -1,23 +1,9 @@
 const Price = require('../models/price');
-//const { Alert } = require('../object/Alert');
 
 class Alert{
-    
     constructor(coin, price){
         this.coin = coin;
         this.price = price;
-    }
-
-    attach(){
-        console.log('entro');
-    }
-
-    detach(){
-        console.log('salgo')
-    }
-
-    notify(){
-        console.log('notifico')
     }
 }
 
@@ -25,9 +11,6 @@ const setAlert = async (req, res) => {
     const { title, price } = req.body;
     const new_price = new Price({ title, price });
     await new_price.save();
-
-    let new_alert = new Alert(title, price);
-    console.log(new_alert.price);
 
     return res.json({status:"Done"});
 }
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,15 @@
 const axios = require('axios');
+const EventEmitter = require('events');
+class MyEmitter extends EventEmitter {}
+const myEmitter = new MyEmitter();
+
+function c1(one, two, three) {
+  console.log(one);
+  console.log(two);
+  console.log(three);
+}
+
+myEmitter.on('eventOne', c1);
 
 const setError = (code, message) => {
     const error = new Error();
@@ -11,9 +22,7 @@ const cryptoPrice = function ()
 {
     axios.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,matic-network&vs_currencies=usd')
     .then(res => { 
-      console.log(Object.values(res.data)[0].usd)
-      console.log(Object.values(res.data)[1].usd)
-      console.log(Object.values(res.data)[2].usd)
+      myEmitter.emit('eventOne',Object.values(res.data)[0].usd, Object.values(res.data)[1].usd, Object.values(res.data)[2].usd)
     })
 }
 
